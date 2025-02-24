80692c5f7393fbf04d44d469617659f753e6c51a
website2l <amanqireshi35@gmail.com>
2025-02-24 10:19:31 +0500

    Create movies-data.js

---
+++ movies-file/movies-data.js
@@ -0,0 +1,130 @@
+
+        const movies = [
+    {
+        "tiltid": "a1b09j",
+        "title": "Nouman Iftikhar Qureshi",
+        "Mainimage": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh_K3nm3XTfNaWzoXgjhXR-YvxPi6ael6gplsqmM8mbFB9cKY-eWmAaKeSQNUcAmLdBOjuhcf9hqcvuGY6x8fqDh-FG53rz3_A-AYLzWmgtal_BCxIcoz9VKQTh1bSo_a5u_IhpoWNQ32p6V8Q9bcxcAN1gUOM4ouGZB1IMnoBEdHspw1s4Mx705EIN9rQ/s1600/Untitled.jpg",
+        "category": "action"
+    },
+    {
+        "tiltid": "gfx6tx9grokX7mak",
+        "title": "Salman Khan",
+        "Mainimage": "https://upload.wikimedia.org/wikipedia/commons/d/dd/Salmanrampwalk.png",
+        "category": "love",
+    },
+    {
+        "tiltid": "coZ3ma8jtL",
+        "title": "Amir Khan",
+        "Mainimage": "https://static.abplive.com/wp-content/uploads/sites/2/2017/04/26115522/aamir-khan.jpg?impolicy=abp_cdn&imwidth=640",
+        "category": "comedy action",
+    },
+    {
+        "tiltid": "Th4s7noX",
+        "title": "Shahrukh Khan",
+        "Mainimage": "https://upload.wikimedia.org/wikipedia/commons/6/6e/Shah_Rukh_Khan_graces_the_launch_of_the_new_Santro.jpg",
+        "category": "action",
+    },
+    {
+        "tiltid": "dPz3n5Y8",
+        "title": "Akshay Kumar",
+        "Mainimage": "https://i.pinimg.com/originals/e7/d3/22/e7d3227bd9c5ebe40ac7dc47c81e2496.jpg",
+        "category": "action comedy",
+    },
+    {
+        "tiltid": "uX6nr9Kt",
+        "title": "Ajay Devgn",
+        "Mainimage": "https://odishasambad.in/wp-content/uploads/2022/04/2022_3largeimg_1928851129.jpg",
+        "category": "horror",
+    },
+    {
+        "tiltid": "Rk4MnX9L",
+        "title": "Ranveer Singh",
+        "Mainimage": "https://via.placeholder.com/200",
+        "category": "action",
+    },
+    {
+        "tiltid": "tZ5Xc9Br",
+        "title": "Ranbir Kapoor",
+        "Mainimage": "https://via.placeholder.com/200",
+        "category": "action",
+    },
+    {
+        "tiltid": "Hw7NbP6X",
+        "title": "Hrithik Roshan",
+        "Mainimage": "https://via.placeholder.com/200",
+        "category": "action",
+    },
+    {
+        "tiltid": "A9Jk7Xp5",
+        "title": "Amitabh Bachchan",
+        "Mainimage": "https://via.placeholder.com/200",
+        "category": "action",
+    },
+    {
+        "tiltid": "Xk7LoZ6N",
+        "title": "Saif Ali Khan",
+        "Mainimage": "https://via.placeholder.com/200",
+        "category": "action",
+    },
+    {
+        "tiltid": "N3P5xX8R",
+        "title": "John Abraham",
+        "Mainimage": "https://via.placeholder.com/200",
+        "category": "action",
+    },
+    {
+        "tiltid": "Xp5L8RbM",
+        "title": "Arjun Kapoor",
+        "Mainimage": "https://via.placeholder.com/200",
+        "category": "action",
+    },
+    {
+        "tiltid": "Mq7X9Lt6",
+        "title": "Varun Dhawan",
+        "Mainimage": "https://via.placeholder.com/200",
+        "category": "action",
+    },
+    {
+        "tiltid": "Lr6X9bN3",
+        "title": "Kartik Aaryan",
+        "Mainimage": "https://via.placeholder.com/200",
+        "category": "action",
+    },
+    {
+        "tiltid": "Z7N5Lb3P",
+        "title": "Tiger Shroff",
+        "Mainimage": "https://via.placeholder.com/200",
+        "category": "action",
+    },
+    {
+        "tiltid": "P7M9LoN6",
+        "title": "Irrfan Khan",
+        "Mainimage": "https://via.placeholder.com/200",
+        "category": "action",
+    },
+    {
+        "tiltid": "karnto123",
+        "title": "max pro Khan",
+        "Mainimage": "https://via.placeholder.com/200",
+        "category": "action",
+    },
+    {
+        "tiltid": "Tiger2Xpo",
+        "title": "Anadi Khan",
+        "Mainimage": "https://via.placeholder.com/200",
+        "category": "action",
+    },
+    {
+        "tiltid": "P7kj9oN6",
+        "title": "Dood Khan",
+        "Mainimage": "https://via.placeholder.com/200",
+        "category": "action",
+    },
+    {
+        "tiltid": "M7M9caseoN6",
+        "title": "Lal Pati",
+        "Mainimage": "https://via.placeholder.com/200",
+        "category": "action",
+    },
+    // Add remaining objects here...
+        ];
