1a85f5ff9e17a6cb70a4364dc97fc523b65f3b81
website2l <amanqireshi35@gmail.com>
2025-02-24 22:20:12 +0500

    Update movies-data.js

--- movies-file/movies-data.js
+++ movies-file/movies-data.js
@@ -1,130 +1,172 @@
 
         const movies = [
     {
-        "tiltid": "a1b09j",
-        "title": "Nouman Iftikhar Qureshi",
-        "Mainimage": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh_K3nm3XTfNaWzoXgjhXR-YvxPi6ael6gplsqmM8mbFB9cKY-eWmAaKeSQNUcAmLdBOjuhcf9hqcvuGY6x8fqDh-FG53rz3_A-AYLzWmgtal_BCxIcoz9VKQTh1bSo_a5u_IhpoWNQ32p6V8Q9bcxcAN1gUOM4ouGZB1IMnoBEdHspw1s4Mx705EIN9rQ/s1600/Untitled.jpg",
-        "category": "action"
+        "tiltid": "Xt3sY45",
+        "title": "Sivaji: The Boss (2007)",
+        "Mainimage": "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/sivaji-the-boss-et00000056-1703755835.jpg",
+        "category": "comedy action",
+        "description": "<p>Sivaji: The Boss is a 2007 Indian Tamil-language Hindi-language action film co-written and directed by S. Shankar and produced by AVM Productions. The film stars Rajinikanth in the titular role alongside Shriya Saran, Vivek and Suman playing pivotal roles with an ensemble supporting cast. In the film, Sivaji is a software systems architect, who returns to India from the US in order to provide free medical treatment and education, but has to face hurdles from the system and Aathisheshan, an influential political leader.</p><br><p>Released on 15 June 2007, Sivaji received positive reviews from critics and became a commercial success by grossing ₹125—160 crore worldwide. It emerged as one of the highest-grossing Indian films at the time of its release and went on to win a National Film Award, three Filmfare Awards and two Vijay Awards. The film was converted into 3D and released on 12 December 2012 as Sivaji 3D. The runtime of the 3D version was shorter than the original, cut to 155 minutes. It also emerged as the first Tamil film to enter the 100 crore club in the history of Kollywood.</p><table><tr><td><span class='bold'>Directed by</span></td><td>S. Shankar</td></tr><tr><td><span class='bold'>Written by</span></td><td>S. Shankar (Story & Screenplay) Sujatha (Dialogues)</td></tr><tr><td><span class='bold'>Produced by</span></td><td>M. S. Guhan M. Saravanan</td></tr><tr><td><span class='bold'>Starring</span></td><td>Rajnikanth Shriya Saran Vivek Suman</td></tr><tr><td><span class='bold'>Cinematography</span></td><td>K. V. Anand</td></tr><tr><td><span class='bold'>Edited by</span></td><td>Anthony</td></tr><tr><td><span class='bold'>Music by</span></td><td>A. R. Rahman</td></tr><tr><td><span class='bold'>Production company</span></td><td>AVM Productions</td></tr><tr><td><span class='bold'>Distributed by</span></td><td>AVM Productions</td></tr><tr><td><span class='bold'>Release dates</span></td><td>14 June 2007 (premiere) 15 June 2007 (India)</td></tr><tr><td><span class='bold'>Running time</span></td><td>188 minutes</td></tr><tr><td><span class='bold'>Country</span></td><td>India</td></tr><tr><td><span class='bold'>Language</span></td><td>Tamil</td></tr><tr><td><span class='bold'>Budget</span></td><td>₹60-89 crore</td></tr><tr><td><span class='bold'>Box office</span></td><td>₹125—160 crore</td></tr></table>",
+        "keywords": ""
     },
     {
         "tiltid": "gfx6tx9grokX7mak",
         "title": "Salman Khan",
         "Mainimage": "https://upload.wikimedia.org/wikipedia/commons/d/dd/Salmanrampwalk.png",
         "category": "love",
+        "description": "Salman Khan (born December 27, 1965, Indore, Madhya Pradesh, India) is an Indian actor, film producer, and television host whose iconic roles, blockbuster hits, and mass appeal have earned him the status of a megastar in Bollywood. Over the years, he has become an inextricable part of popular culture in India. Khan, who started out as a model and actor in the late 1980s, has appeared in more than 100 movies and boasts a career spanning almost four decades. Along with actors Shah Rukh Khan and Aamir Khan, Salman Khan is one of the three most successful male stars of Bollywood since the 1990s. Members of his die-hard fan base affectionately address him as Bhai (Hindi: “Brother”) or Bhaijaan (a respectful and loving address for “brother” in Urdu).",
+        "keywords": ""
     },
     {
         "tiltid": "coZ3ma8jtL",
         "title": "Amir Khan",
         "Mainimage": "https://static.abplive.com/wp-content/uploads/sites/2/2017/04/26115522/aamir-khan.jpg?impolicy=abp_cdn&imwidth=640",
         "category": "comedy action",
+        "description": "",
+        "keywords": "Yaadon , Ki , Baaraat , Holi , Madhosh , Manzil"
     },
     {
         "tiltid": "Th4s7noX",
         "title": "Shahrukh Khan",
         "Mainimage": "https://upload.wikimedia.org/wikipedia/commons/6/6e/Shah_Rukh_Khan_graces_the_launch_of_the_new_Santro.jpg",
         "category": "action",
+        "description": "",
+        "keywords": ""
     },
     {
         "tiltid": "dPz3n5Y8",
         "title": "Akshay Kumar",
         "Mainimage": "https://i.pinimg.com/originals/e7/d3/22/e7d3227bd9c5ebe40ac7dc47c81e2496.jpg",
         "category": "action comedy",
+        "description": "",
+        "keywords": ""
     },
     {
         "tiltid": "uX6nr9Kt",
         "title": "Ajay Devgn",
         "Mainimage": "https://odishasambad.in/wp-content/uploads/2022/04/2022_3largeimg_1928851129.jpg",
         "category": "horror",
+        "description": "",
+        "keywords": ""
     },
     {
         "tiltid": "Rk4MnX9L",
         "title": "Ranveer Singh",
         "Mainimage": "https://via.placeholder.com/200",
         "category": "action",
+        "description": "",
+        "keywords": ""
     },
     {
         "tiltid": "tZ5Xc9Br",
         "title": "Ranbir Kapoor",
         "Mainimage": "https://via.placeholder.com/200",
         "category": "action",
+        "description": "",
+        "keywords": ""
     },
     {
         "tiltid": "Hw7NbP6X",
         "title": "Hrithik Roshan",
         "Mainimage": "https://via.placeholder.com/200",
         "category": "action",
+        "description": "",
+        "keywords": ""
     },
     {
         "tiltid": "A9Jk7Xp5",
         "title": "Amitabh Bachchan",
         "Mainimage": "https://via.placeholder.com/200",
         "category": "action",
+        "description": "",
+        "keywords": ""
     },
     {
         "tiltid": "Xk7LoZ6N",
         "title": "Saif Ali Khan",
         "Mainimage": "https://via.placeholder.com/200",
         "category": "action",
+        "description": "",
+        "keywords": ""
     },
     {
         "tiltid": "N3P5xX8R",
         "title": "John Abraham",
         "Mainimage": "https://via.placeholder.com/200",
         "category": "action",
+        "description": "",
+        "keywords": ""
     },
     {
         "tiltid": "Xp5L8RbM",
         "title": "Arjun Kapoor",
         "Mainimage": "https://via.placeholder.com/200",
         "category": "action",
+        "description": "",
+        "keywords": ""
     },
     {
         "tiltid": "Mq7X9Lt6",
         "title": "Varun Dhawan",
         "Mainimage": "https://via.placeholder.com/200",
         "category": "action",
+        "description": "",
+        "keywords": ""
     },
     {
         "tiltid": "Lr6X9bN3",
         "title": "Kartik Aaryan",
         "Mainimage": "https://via.placeholder.com/200",
         "category": "action",
+        "description": "",
+        "keywords": ""
     },
     {
         "tiltid": "Z7N5Lb3P",
         "title": "Tiger Shroff",
         "Mainimage": "https://via.placeholder.com/200",
         "category": "action",
+        "description": "",
+        "keywords": ""
     },
     {
         "tiltid": "P7M9LoN6",
         "title": "Irrfan Khan",
         "Mainimage": "https://via.placeholder.com/200",
         "category": "action",
+        "description": "",
+        "keywords": ""
     },
     {
         "tiltid": "karnto123",
         "title": "max pro Khan",
         "Mainimage": "https://via.placeholder.com/200",
         "category": "action",
+        "description": "",
+        "keywords": ""
     },
     {
         "tiltid": "Tiger2Xpo",
         "title": "Anadi Khan",
         "Mainimage": "https://via.placeholder.com/200",
         "category": "action",
+        "description": "",
+        "keywords": ""
     },
     {
         "tiltid": "P7kj9oN6",
         "title": "Dood Khan",
         "Mainimage": "https://via.placeholder.com/200",
         "category": "action",
+        "description": "",
+        "keywords": ""
     },
     {
         "tiltid": "M7M9caseoN6",
         "title": "Lal Pati",
         "Mainimage": "https://via.placeholder.com/200",
         "category": "action",
+        "description": "",
+        "keywords": ""
     },
     // Add remaining objects here...
         ];
