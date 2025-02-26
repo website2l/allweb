d51e6e06edbc90cefce914b39a1323b8fe7f4029
website2l <amanqireshi35@gmail.com>
2025-02-26 23:11:38 +0500

    Update description-css-imran-riaz-khan-video.js

--- movies-file/description-css-imran-riaz-khan-video.js
+++ movies-file/description-css-imran-riaz-khan-video.js
@@ -1,1 +1,8 @@
 
+    // Simulating content load after 2 seconds
+    setTimeout(function() {
+        document.querySelector('.skeleton-iframe').style.display = 'none';
+        document.querySelector('.skeleton-heading').style.display = 'none';
+        document.querySelector('iframe').style.display = 'block';
+        document.querySelector('h2 span').style.display = 'block';
+    }, 2000);
