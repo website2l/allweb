772f99bc0a155b5b9e0215433f20786387e8cd40
website2l <amanqireshi35@gmail.com>
2025-03-04 22:45:42 +0500

    Update channels-banner-data.js

--- movies-file/channels-banner-data.js
+++ movies-file/channels-banner-data.js
@@ -3,4 +3,11 @@
         const validChannels = {
             "Dezni": "https://website2l.github.io/allweb/movies-file/denzi-channel.css",
             "Bollywood Hits": "https://website2l.github.io/allweb/movies-file/Bollywood-Hits.css",
+            "NiQ": "https://website2l.github.io/allweb/movies-file/NiQ-Cannel-Benner.css",
+            "T-Series": "https://website2l.github.io/allweb/movies-file/T-Series-Channel-Benner.css",
+            "Sony": "https://website2l.github.io/allweb/movies-file/Sony-Channel-Banner.css",
+            "Max": "https://website2l.github.io/allweb/movies-file/Max-Channel-Banner.css",
+            "RedRabit": "https://website2l.github.io/allweb/movies-file/RedRabit-Channel-Banner.css",
+            "ARY NEWS": "https://website2l.github.io/allweb/movies-file/ARY-NEWS-Channel-Banner.css",
+            "Green-TV": "https://website2l.github.io/allweb/movies-file/Green-TV-Channel-Banner.css",
         };
