9649a0ebeef49e92649672a461b6593b09cb341f
website2l <amanqireshi35@gmail.com>
2025-03-04 21:26:02 +0500

    Create channels-banner-data.js

---
+++ movies-file/channels-banner-data.js
@@ -0,0 +1,6 @@
+
+        // ✅ Valid Channels List
+        const validChannels = {
+            "Dezni": "https://website2l.github.io/allweb/movies-file/denzi-channel.css",
+            "Bollywood Hits": "https://website2l.github.io/allweb/movies-file/Bollywood-Hits.css",
+        };
